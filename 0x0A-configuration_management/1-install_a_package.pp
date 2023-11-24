421b17041fb01d04e0eff95753bb29d9130a7d98
kahenyamercy <kahenyamercy5@gmail.com>
2023-11-24 14:56:07 +0000

    install  package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# Install FAlask package
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => '/usr/bin',
-  unless  => '/usr/bin/pip3 show Flask | grep Version | grep 2.1.0',
+#Install flask version 2.1.0
+
+
+exec {'pip3 install flask':
+require => Exec['python-installed'],
+command => '/usr/bin/pip3 install flask==2.1.0'
+}
+
+exec {'python-installed':
+command => '/usr/bin/which python3'
 }
