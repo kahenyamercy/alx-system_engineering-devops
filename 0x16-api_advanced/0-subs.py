6a51552c44781fa58851e7052d41823be2def405
kahenyamercy <kahenyamercy5@gmail.com>
2024-01-17 08:08:05 +0000

    updates

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,6 +6,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
+    if not subreddit:
+        return 0
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
